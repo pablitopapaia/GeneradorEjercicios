0eb152a394ef6004e300546a5a4e26a5d0475cea
Eustaquio <estaqix@hotmail.com>
2023-02-23 20:01:29 +0100

    se ha añadido la opción de modificar el fichero de configuracion

--- Proyecto/Window3.xaml.cs
+++ Proyecto/Window3.xaml.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.IO;
+using System.Configuration;
+using System;
+
 
 namespace Proyecto
 {
@@ -23,12 +15,17 @@ namespace Proyecto
         {
             InitializeComponent();
             ResizeMode = ResizeMode.NoResize;
-            
+            string miClave = ConfigurationManager.AppSettings["BackgroundColor"];
+            string miClave2 = ConfigurationManager.AppSettings["ButtonColor"];
+            editFile.Text = miClave;
 
         }
 
         private void guardar(object sender, RoutedEventArgs e)
         {
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            config.AppSettings.Settings["BackgroundColor"].Value = editFile.Text;
+            config.Save();
             Close();
         }
     }
